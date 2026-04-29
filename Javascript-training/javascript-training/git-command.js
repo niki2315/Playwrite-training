476dd0275a8e1a6940ef803d1f2bfb96312b0679
niki2315 <nikitasrvstv1997@gmail.com>
2026-04-29 17:23:33 +0530

    added git command file

---
+++ Javascript-training/javascript-training/git-command.js
@@ -0,0 +1,27 @@
+/* Git Command
+
+ 
+
+step 1- git init
+step 2 - git remote add origin https://github.com/shivginilytics/Assignment.git
+step 3 - git status
+step 4 - git add <filename> or git .
+step 5 - git commit -m "message"
+Step 6 - git config --global user.email "email@gmail.com"
+Step 7 - git config --global user.name "<username>"
+Step 8 - git push origin master
+
+ 
+
+--------------------------------------------------------------------------------
+
+ 
+
+Daily use command
+
+ 
+
+git status
+git add .
+git commit -m "message"
+git push origin master */
